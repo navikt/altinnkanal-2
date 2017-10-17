8ce4a42cb9048c9322a7034c41e21d1ce5f4b673
Kevin Sillerud <kevin.sillerud@nav.no>
2017-10-17 09:29:57 +0200

    Add pure and allow it to be downloaded

---
+++ src/main/java/no/nav/altinnkanal/config/MvcConfiguration.java
@@ -0,0 +1,17 @@
+package no.nav.altinnkanal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/css/**")
+                .addResourceLocations("/css/");
+    }
+}
