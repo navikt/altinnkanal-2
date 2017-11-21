a86df04adcb347af9738d0f52896763478264345
Nguyen, Trong Huu <trong.huu.nguyen@nav.no>
2017-11-21 13:41:54 +0100

    Expose default JVM metrics to Prometheus

--- src/main/java/no/nav/altinnkanal/BootstrapROBEA.java
+++ src/main/java/no/nav/altinnkanal/BootstrapROBEA.java
@@ -1,5 +1,6 @@
 package no.nav.altinnkanal;
 
+import io.prometheus.client.hotspot.DefaultExports;
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootstrapROBEA {
 
     public static void main(String[] args) {
+        DefaultExports.initialize();
         SpringApplication.run(BootstrapROBEA.class, args);
     }
 }
