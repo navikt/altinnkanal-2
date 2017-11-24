b2e2a75c068e5dc37e552088e8a83da4a7ee57df
Kevin Sillerud <kevin.sillerud@nav.no>
2017-11-24 11:24:33 +0100

    Don't initialize a GenericRecord

--- src/main/java/no/nav/altinnkanal/services/KafkaServiceImpl.java
+++ src/main/java/no/nav/altinnkanal/services/KafkaServiceImpl.java
@@ -1,8 +1,6 @@
 package no.nav.altinnkanal.services;
 
 import no.nav.altinnkanal.avro.ExternalAttachment;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericRecord;
 import org.apache.commons.lang.SerializationException;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -25,12 +23,7 @@ public class KafkaServiceImpl implements KafkaService {
 
     @Override
     public Future<RecordMetadata> publish(String topic, ExternalAttachment externalAttachment) throws IOException, SerializationException {
-        GenericRecord avroRecord = new GenericData.Record(externalAttachment.getSchema());
-        avroRecord.put("batch", externalAttachment.getBatch());
-        avroRecord.put("sc", externalAttachment.getSc());
-        avroRecord.put("sec", externalAttachment.getSec());
-        avroRecord.put("archRef", externalAttachment.getArchRef());
-        return producer.send(new ProducerRecord<>(topic, avroRecord));
+        return producer.send(new ProducerRecord<>(topic, externalAttachment));
     }
 
     @PreDestroy
